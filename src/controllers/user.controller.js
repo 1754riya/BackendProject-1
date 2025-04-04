4b3ac04b96577f4cd13f1234b69fd84e22d8dc81
1754riya <rmehta1754@gmail.com>
2025-04-05 00:53:23 +0530

    commit changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,16 +23,6 @@ const generateAccessAndRefreshTokens = async(userId)=>{
     }
 }
 const registerUser = asyncHandler( async (req, res) => {
-  // get user details from frontend
-  // validation - not empty
-  // check if user already exists: username, email
-  // check for images, check for avatar
-  // upload them to cloudinary, avatar
-  // create user object - create entry in db
-  // remove password and refresh token field from response
-  // check for user creation
-  // return res
-
 
   const {fullName, email, username, password } = req.body;
   //console.log("email: ", email);
@@ -106,12 +96,6 @@ const registerUser = asyncHandler( async (req, res) => {
 console.log('Is registerUser defined in controller:', typeof registerUser);
 
 const loginUser = asyncHandler(async(req,res)=>{
-// req body ->data
-// username or email
-// find the user
-// password check
-// access and refresh token
-// send cookies 
 const {email,username,password} =  req.body;
 console.log("Received Login Request:", req.body);
 if(!username && !email){
@@ -125,7 +109,7 @@ console.log("User Found:", user);
 if(!user){
     throw new ApiError(404,"user does not exist")
 }
-const isPasswordValid= user.isPasswordCorrect (password)
+const isPasswordValid= await user.isPasswordCorrect (password)
 if (!isPasswordValid) {
     throw new ApiError(401, "Invalid user credentials")
     }
@@ -163,7 +147,7 @@ const logoutUser = asyncHandler(async(req, res) => {
         req.user._id,
         {
             $unset: {
-                refreshToken: 1 // this removes the field from document
+                refreshToken: 1 
             }
         },
         {

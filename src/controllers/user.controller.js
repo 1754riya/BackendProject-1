d3b3655dc254d16f5cb8ed76a0f69b5dbcf8ae77
1754riya <rmehta1754@gmail.com>
2025-04-05 02:18:32 +0530

    commit changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,16 +26,6 @@ const generateAccessAndRefreshTokens = async(userId)=>{
     }
 }
 const registerUser = asyncHandler( async (req, res) => {
-  // get user details from frontend
-  // validation - not empty
-  // check if user already exists: username, email
-  // check for images, check for avatar
-  // upload them to cloudinary, avatar
-  // create user object - create entry in db
-  // remove password and refresh token field from response
-  // check for user creation
-  // return res
-
 
   const {fullName, email, username, password } = req.body;
   //console.log("email: ", email);
@@ -110,14 +100,8 @@ const registerUser = asyncHandler( async (req, res) => {
 console.log('Is registerUser defined in controller:', typeof registerUser);
 
 const loginUser = asyncHandler(async(req,res)=>{
-// req body ->data
-// username or email
-// find the user
-// password check
-// access and refresh token
-// send cookies
 const {email,username,password} =  req.body;
-// console.log("Received Login Request:", req.body);
+console.log("Received Login Request:", req.body);
 if(!username && !email){
     throw new ApiError (400,"username or email is required")
 }
@@ -127,8 +111,8 @@ const user = await User.findOne({
 if(!user){
     throw new ApiError(404,"user does not exist")
 }
-    const isPasswordValid = user.isPasswordCorrect(password);
-    // console.log("Is Password Valid:", isPasswordValid);
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    console.log("Is Password Valid:", isPasswordValid);
 if (!isPasswordValid) {
     throw new ApiError(401, "Invalid user credentials")
     }
@@ -166,7 +150,7 @@ const logoutUser = asyncHandler(async(req, res) => {
         req.user._id,
         {
             $unset: {
-                refreshToken: 1 // this removes the field from document
+                refreshToken: 1 
             }
         },
         {

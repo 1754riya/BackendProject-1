6957690597e954d9ec665c6cab4ac35598045dc4
1754riya <rmehta1754@gmail.com>
2025-04-09 18:50:37 +0530

    Commit Changes

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,7 +1,7 @@
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { Subscription } from "../models/Subscription.model.js";
+import {subscription} from "../models/subscription.model.js";
 import { User } from "../models/user.model.js";
 import mongoose, { isValidObjectId } from "mongoose";
 
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,10 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import {
-  uploadOnCloudinary,
-  deleteFromCloudinary,
-} from "../utils/upload.cloudinary.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+  
 import { Video } from "../models/Video.model.js";
 import { User } from "../models/user.model.js";
 import mongoose, { isValidObjectId } from "mongoose";

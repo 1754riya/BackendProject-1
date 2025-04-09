1ed30971d308ce7a3204dea37f6b53fa81963ec8
1754riya <rmehta1754@gmail.com>
2025-04-09 18:37:46 +0530

    routes, controller committed

--- src/app.js
+++ src/app.js
@@ -14,9 +14,26 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+import commentRouter from "./routes/comment.routes.js";
+import HealthCheckRouter from "./routes/healthcheck.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
+
 //routes declaration
 
+
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/healthCheck", HealthCheckRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
 
 // http://localhost:8000/api/v1/users/register
 
